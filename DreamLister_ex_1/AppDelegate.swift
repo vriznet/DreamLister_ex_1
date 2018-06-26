794ab7f5b731368be0f4732da86ac6295537df44
vriznet <vriz@vriz.net>
2018-06-26 13:16:33 +0900

    AppDelegate Shortcut task finished

--- DreamLister_ex_1/AppDelegate.swift
+++ DreamLister_ex_1/AppDelegate.swift
@@ -88,6 +88,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             }
         }
     }
-
 }
-
+let ad = UIApplication.shared.delegate as! AppDelegate
+let context = ad.persistentContainer.viewContext
